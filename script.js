dbb75b4146bb6f2f85d1d33dfc5115ee40cfb57b
Carlo Ghelfi <ghelfighel@gmail.com>
2025-07-15 12:49:23 +0200

    sito web

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById("buy-button").addEventListener("click", function () {
+  window.location.href = "https://checkout.cleeng.com/"; // Inserisci qui il link dell'offerta Cleeng
+});
