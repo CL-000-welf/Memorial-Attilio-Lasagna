c98770e10cb6c05fdebbd4e1262e7e4a62a1f421
Carlo Ghelfi <ghelfighel@gmail.com>
2025-07-18 09:16:57 +0200

    Update

--- script.js
+++ script.js
@@ -1,36 +1,40 @@
 document.getElementById("payWithPaypal").addEventListener("click", async () => {
-    const offerId = "E901153399_IT";
+    const offerId = "E742054483_IT";
     const paymentMethodId = "288978806";
     const redirectUrl = window.location.origin + "/thankyou.html";
 
     try {
-        const orderRes = await fetch("https://api.cleeng.com/3.1/orders", {
+        const orderRes = await fetch("https://sandbox-api.cleeng.com/3.1/orders", {
             method: "POST",
             headers: {
-                "Content-Type": "application/json"
+                "Content-Type": "application/json",
+                "Authorization": "ApiKey d5YHMSLo1MrGctScuB5iRF358hS3jpd5ezlsf-WTqgGrqNw8"
             },
             body: JSON.stringify({
-                offerId: offerId
+                offerId: offerId,
+                publisherId: 620638530
             })
         });
 
         const orderData = await orderRes.json();
         const orderId = orderData.orderId;
 
-        await fetch(`https://api.cleeng.com/3.1/orders/${orderId}`, {
+        await fetch(`https://sandbox-api.cleeng.com/3.1/orders/${orderId}`, {
             method: "PATCH",
             headers: {
-                "Content-Type": "application/json"
+                "Content-Type": "application/json",
+                "Authorization": "ApiKey d5YHMSLo1MrGctScuB5iRF358hS3jpd5ezlsf-WTqgGrqNw8"
             },
             body: JSON.stringify({
                 paymentMethodId: paymentMethodId
             })
         });
 
-        const tokenRes = await fetch("https://api.cleeng.com/connectors/paypal/v1/tokens", {
+        const tokenRes = await fetch("https://sandbox-api.cleeng.com/connectors/paypal/v1/tokens", {
             method: "POST",
             headers: {
-                "Content-Type": "application/json"
+                "Content-Type": "application/json",
+                "Authorization": "ApiKey d5YHMSLo1MrGctScuB5iRF358hS3jpd5ezlsf-WTqgGrqNw8"
             },
             body: JSON.stringify({
                 orderId: orderId,
